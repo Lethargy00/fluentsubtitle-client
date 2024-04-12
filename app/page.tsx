61b6c9200d009141b785281ce30eccba63bafb0e
Lethargy00 <142389181+Lethargy00@users.noreply.github.com>
2024-04-12 08:22:50 +0200

    Fixed start menu buttons

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,10 @@ export default function Home() {
   return (
     <main className="flex items-center">
       <h1 className="text-3xl text-center">Welcome to FluentSubtitle the #1 site for downloading subtitles</h1>
-      <Link href="./list" className="text-2xl text-center underline hover:text-gray-600">Movie List</Link>
-      <Link href="./contact" className="text-2xl text-center underline hover:text-gray-600">Contact information</Link>
+      <div className="flex gap-5 mt-10">
+        <Link href="./list" className="text-2xl text-center underline border-zinc-700 border-8 rounded-md bg-zinc-700 bd hover:text-gray-600">Movie List</Link>
+        <Link href="./contact" className="text-2xl text-center underline  border-zinc-700 border-8 rounded-md bg-zinc-700 hover:text-gray-600">Contact information</Link>
+      </div>
     </main>
   );
 }
