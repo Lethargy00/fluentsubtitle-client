138b50e35512eb26e2f897076288ad78ac0a1345
Lethargy00 <142389181+Lethargy00@users.noreply.github.com>
2024-05-15 08:30:35 +0200

    Added feedback on form

--- app/list/[movieId]/components/SubtitleForm.tsx
+++ app/list/[movieId]/components/SubtitleForm.tsx
@@ -20,49 +20,83 @@ const SubtitleForm: React.FC<SubtitleFormProps> = ({
   const [selectedLanguage, setSelectedLanguage] = useState(languages[0]);
   const [isForHearingImpaired, setIsForHearingImpaired] = useState(false);
   const [subtitleFile, setSubtitleFile] = useState<File | null>(null);
+  const [errors, setErrors] = useState({
+    uploaderName: "",
+    subtitleFile: "",
+  });
 
   const handleLanguageChange = (selectedOption: any) => {
     setSelectedLanguage(selectedOption);
   };
 
+  const validateForm = () => {
+    let newErrors: { uploaderName: string; subtitleFile: string } = {
+      uploaderName: "",
+      subtitleFile: "",
+    };
+
+    if (uploaderName.trim() === "") {
+      newErrors.uploaderName = "Username is required";
+    }
+
+    if (!subtitleFile) {
+      newErrors.subtitleFile = "Subtitle file is required";
+    }
+
+    setErrors(newErrors);
+
+    return Object.values(newErrors).every((error) => error === "");
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setSubtitleFile(e.target.files[0]);
+    } else {
+      setSubtitleFile(null);
+    }
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (validateForm()) {
+      if (subtitleFile) {
+        const formData = new FormData();
+        formData.append("subtitleFile", subtitleFile);
 
-    if (subtitleFile) {
-      const formData = new FormData();
-      formData.append("subtitleFile", subtitleFile);
+        try {
+          console.log("File name:", subtitleFile.name); // Log the file name
+          const fileExtension = subtitleFile.name.split(".").pop();
+          console.log("File extension:", fileExtension); // Log the extracted file extension
 
-      try {
-        console.log("File name:", subtitleFile.name); // Log the file name
-        const fileExtension = subtitleFile.name.split(".").pop();
-        console.log("File extension:", fileExtension); // Log the extracted file extension
+          const subtitleData: Subtitle = {
+            id: Date.now().toString(), // Generate a unique ID for the subtitle
+            uploaderName: uploaderName,
+            language: selectedLanguage.value,
+            isForHearingImpaired: isForHearingImpaired,
+            uploadedDate: new Date().toISOString(),
+            subtitleFileName: subtitleFile.name,
+            subtitleFileSize: subtitleFile.size,
+            subtitleFileType: fileExtension || "",
+          };
 
-        const subtitleData: Subtitle = {
-          id: Date.now().toString(), // Generate a unique ID for the subtitle
-          uploaderName: uploaderName,
-          language: selectedLanguage.value,
-          isForHearingImpaired: isForHearingImpaired,
-          uploadedDate: new Date().toISOString(),
-          subtitleFileName: subtitleFile.name,
-          subtitleFileSize: subtitleFile.size,
-          subtitleFileType: fileExtension || "",
-        };
+          addSubtitleHandler(subtitleData);
 
-        addSubtitleHandler(subtitleData);
-
-        onFormClose();
-      } catch (error) {
-        console.error("Error saving subtitle data to IndexedDB", error);
+          onFormClose();
+        } catch (error) {
+          console.error("Error saving subtitle data to IndexedDB", error);
+        }
+      } else {
+        console.error("No subtitle file selected");
       }
-    } else {
-      console.error("No subtitle file selected");
     }
   };
 
   return (
     <form onSubmit={handleSubmit} className={style.formContainer}>
       <h2 className={style.headerText}>Upload subtitle</h2>
-
+      {errors.uploaderName && (
+        <p className={style.error}>{errors.uploaderName}</p>
+      )}
       <input
         type="text"
         placeholder="Username"
@@ -70,12 +104,13 @@ const SubtitleForm: React.FC<SubtitleFormProps> = ({
         value={uploaderName}
         onChange={(e) => setUploaderName(e.target.value)}
       />
+
       <Select
-        value={selectedLanguage} // Current selected language.
+        value={selectedLanguage}
         className={style.dropDown}
-        options={languages} // Options to choose from.
-        onChange={handleLanguageChange} // Handler for when the selection changes.
-        isSearchable={false} // Removes search function.
+        options={languages}
+        onChange={handleLanguageChange}
+        isSearchable={false}
       />
       <label>
         <input
@@ -86,11 +121,11 @@ const SubtitleForm: React.FC<SubtitleFormProps> = ({
         For Hearing Impaired
       </label>
 
-      <input
-        type="file"
-        accept=".srt"
-        onChange={(e) => setSubtitleFile(e.target.files![0])}
-      />
+      {errors.subtitleFile && (
+        <p className={style.error}>{errors.subtitleFile}</p>
+      )}
+
+      <input type="file" accept=".srt" onChange={handleFileChange} />
       <div className={style.buttonContainer}>
         <button type="submit" className={style.upload}>
           Upload
