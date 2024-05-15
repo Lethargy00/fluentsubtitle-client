489ab99f0b0078d8fa3422658f3e2ea2a11a3d6a
Lethargy00 <142389181+Lethargy00@users.noreply.github.com>
2024-05-15 10:55:16 +0200

    Updated comments

--- app/api/search.ts
+++ app/api/search.ts
@@ -5,22 +5,32 @@ interface UsePerformSearchResult {
   performSearch: () => Promise<void>;
 }
 
+// Custom hook to perform a movie search using The Movie Database API
 export const usePerformSearch = (
   searchQuery: string
 ): UsePerformSearchResult => {
+  // State to hold the search results
   const [results, setResults] = useState<any[]>([]);
 
+  // Function to perform the search.
   const performSearch = async () => {
     if (searchQuery.trim() !== "") {
       const apiKey = "4421cf94a11108972354abe55a5af248";
-      const response = await fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchQuery}`
-      );
-      const data = await response.json();
-      // console.log(data.results);
-      setResults(data.results); // Update the results state with the fetched data.
+      try {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchQuery}`
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setResults(data.results);
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+      }
     }
   };
 
+  // Return the search results and the performSearch function.
   return { results, performSearch };
 };
